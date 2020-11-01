dc0f00932ddf22a69d76ff80c3baa0623eef9c9d
Олег Есиков <esikov.oleg@mail.ru>
2020-11-01 19:30:19 +0300

    Добавление фронтенда

---
+++ frontend/src/components/Journal.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>Sea Voyages Journal</h1>
+        <button>ADD NEW RECORD</button>
+        <button>RECORDS FILTER AND SORT</button>
+        <button>STATISTICS</button>
+        <button>BACK-UP USAGE</button>
+        <table>
+            <tr>
+                <th>Ship Name</th>
+                <th>Commander</th>
+                <th>Departure</th>
+                <th>Destination</th>
+                <th>Start Date</th>
+                <th>End Date</th>
+                <th>Distance, km</th>
+                <th></th>
+            </tr>
+            <tr>
+                <td>LEWIS</td>
+                <td>D.HUTCHINSON</td>
+                <td>SALEM</td>
+                <td>ZANZIBAR</td>
+                <td>1854/01/01</td>
+                <td>1854/02/01</td>
+                <td>465</td>
+                <td>
+                    <button>Delete</button>
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Journal"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/Record.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Sea Voyages Journal: Record №{{ this.$route.params.id }}</h1>
+</template>
+
+<script>
+    export default {
+        name: "Record"
+    }
+</script>
+
+<style scoped>
+
+</style>
