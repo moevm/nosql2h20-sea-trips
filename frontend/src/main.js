8bc59e1ea66505a24f02992bfb50eef008dc0b1a
Олег Есиков <esikov.oleg@mail.ru>
2020-11-02 17:34:56 +0300

    Вынесение роутера в отдельный файл

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,26 +1,6 @@
-import Vue from 'vue'
-import VueRouter from "vue-router";
-import App from './App.vue'
-import Statistics from "./components/Statistics";
-import Record from "./components/Record";
-import NotFound from "./components/NotFound";
-import Journal from "./components/Journal";
-
-Vue.use(VueRouter);
-
-const routes = [
-  { path: '', component: Journal },
-  { path: '/statistics', component: Statistics },
-  { path: '/record/:id', component: Record },
-  { path: '*', component: NotFound}
-];
-
-const router = new VueRouter({
-  routes,
-  mode: 'history'
-});
-
-//Vue.config.productionTip = false;
+import Vue from 'vue';
+import App from './App.vue';
+import router from "./router";
 
 new Vue({
   router,
